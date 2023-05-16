d496bd1a74a8b718d4d57b2bf7a33d7ed8532b12
faithfulsupremacy <130833458+faithfulsupremacy@users.noreply.github.com>
2023-05-16 20:02:48 +0300

    Add files via upload

--- theme.js
+++ theme.js
@@ -1,14 +1,23 @@
 var mode = 1 //Tumšais režīms
+var labaFrame = parent.frames['laba'];
+var labaFrameDocument = labaFrame.document;
+var apaksaFrameDocument = 
 
 function theme() {
+
+    
 
     if (mode == 1) { //Tumšais režīms
         document.getElementById("themeButton").innerHTML = "Gaišais režīms";
         document.body.style.backgroundColor = "navy";   //1.html = midnightblue
+        labaFrameDocument.body.style.backgroundColor = 'midnightblue';
         mode = 2
     } else { //Gaišais režīms
         document.getElementById("themeButton").innerHTML = "Tumšais režīms";
         document.body.style.backgroundColor = "gainsboro";
+        labaFrameDocument.body.style.backgroundColor = 'aliceblue';
         mode = 1
     }
 }
+//313131 202020 (3.)
+//454545 4c4c4c (1.)
